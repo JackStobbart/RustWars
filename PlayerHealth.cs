d6d388296dc676c5f54c135cb06654c9dfb75586
JackStobbart <JackStobbart@live.co.uk>
2017-04-27 20:02:43 +0100

    Add files via upload

---
+++ PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float currentPlayerHealth;
+    public float startingPlayerHealth;
+
+    public GameObject player;
+    public Image healthBar;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GameObject thePlayer = GameObject.FindGameObjectWithTag("PlayerCollision");
+        BasicTankHealth playerScript = thePlayer.GetComponent<BasicTankHealth>();
+        currentPlayerHealth = playerScript.currentHealth;
+        startingPlayerHealth = playerScript.startingHealth;
+
+        healthBar.fillAmount = currentPlayerHealth / startingPlayerHealth;
+
+    }
+}
---
+++ basicTankVFX.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicTankVFX : MonoBehaviour {
+
+    public AudioSource fire;
+    public ParticleSystem firing;
+    public ParticleSystem smoke;
+
+    public void Fire()
+    {
+        fire.Play();
+        Instantiate(firing, transform.position, transform.rotation);
+        Instantiate(smoke, transform.position, transform.rotation);
+    }
+}
