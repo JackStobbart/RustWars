b5a73ab5afaa58e5ccaa1a19928274dd1bfecc0a
JackStobbart <JackStobbart@live.co.uk>
2017-04-27 20:00:25 +0100

    Add files via upload

---
+++ Collapse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collapse : MonoBehaviour {
+
+    public ParticleSystem Collapsing;
+
+    public GameObject Smoke1;
+    public GameObject Smoke2;
+    public GameObject Smoke3;
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(Collapsing, Smoke1.transform.position, Smoke1.transform.rotation);
+        Instantiate(Collapsing, Smoke2.transform.position, Smoke2.transform.rotation);
+        Instantiate(Collapsing, Smoke3.transform.position, Smoke3.transform.rotation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Destoyed.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destoyed : MonoBehaviour {
+
+    public bool sink;
+
+    public float speed = 10;
+    public Transform target;
+
+    public AudioSource destroyed;
+
+    // Use this for initialization
+    void Start() {
+        destroyed.Play();
+        sink = true;
+        StartCoroutine (Destroy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sink == true)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+                
+    }
+
+    IEnumerator Destroy()
+    {
+
+    yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+
+    }
+}
